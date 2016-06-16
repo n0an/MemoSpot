1a8fd6b9069b61ac80660dbccfeb741ccf490063
n0an <nantog@gmail.com>
2016-06-16 21:59:11 +0300

    LocationVC: segue added, heightForRowAtIndexPath added

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -239,6 +239,23 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
         
     }
     
+    
+    // MARK: - NAVIGATION
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        if segue.identifier == "TagLocation" {
+            let navigationController = segue.destinationViewController as! UINavigationController
+            
+            let controller = navigationController.topViewController as! LocationDetailsViewController
+            
+            controller.coordinate = location!.coordinate
+            controller.placemark = placemark
+            
+        }
+        
+    }
+    
   
     
     
--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -7,6 +7,18 @@
 //
 
 import UIKit
+import CoreLocation
+
+private let dateFormatter: NSDateFormatter = {
+    
+    let formatter = NSDateFormatter()
+    formatter.dateStyle = .MediumStyle
+    formatter.timeStyle = .ShortStyle
+    
+    print("return formatter")
+    
+    return formatter
+}()
 
 class LocationDetailsViewController: UITableViewController {
     
@@ -18,6 +30,11 @@ class LocationDetailsViewController: UITableViewController {
     @IBOutlet weak var longitudeLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
+    
+    // MARK: - ATTRIBUTES
+
+    var coordinate = CLLocationCoordinate2D(latitude: 0, longitude: 0)
+    var placemark: CLPlacemark?
 
 
     // MARK: - viewDidLoad
@@ -25,11 +42,64 @@ class LocationDetailsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        descriptionTextView.text = ""
+        categoryLabel.text = ""
+        
+        latitudeLabel.text = String(format: "%.8f", coordinate.latitude)
+        longitudeLabel.text = String(format: "%.8f", coordinate.longitude)
+        
+        if let placemark = placemark {
+            addressLabel.text = stringFromPlacemark(placemark)
+            
+        } else {
+            addressLabel.text = "No Address Found"
+        }
+        
+        dateLabel.text = formatDate(NSDate())
+        
+        
     }
 
    
     // MARK: - HELPER METHODS
+    
+    func stringFromPlacemark(placemark: CLPlacemark) -> String {
+        
+        var text = ""
+        
+        if let s = placemark.subThoroughfare {
+            text += s + " "
+        }
+        
+        if let s = placemark.thoroughfare {
+            text += s
+        }
+        
+        if let s = placemark.locality {
+            text += s + " "
+        }
+        
+        if let s = placemark.administrativeArea {
+            text += s + " "
+        }
+        
+        if let s = placemark.postalCode {
+            text += s
+        }
+        
+        if let s = placemark.country {
+            text += s
+        }
+        
+        return text
+        
+    }
+    
+    func formatDate(date: NSDate) -> String {
+        return dateFormatter.stringFromDate(date)
+    }
+    
+    
 
 
     // MARK: - ACTIONS
@@ -41,5 +111,49 @@ class LocationDetailsViewController: UITableViewController {
     @IBAction func cancel() {
         dismissViewControllerAnimated(true, completion: nil)
     }
+    
+    
+    
+    
+    // MARK: - UITableViewDelegate
+
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        
+        if indexPath.section == 0 && indexPath.row == 0 {
+            return 88
+        } else if indexPath.section == 2 && indexPath.row == 2 {
+            
+//            addressLabel.frame.size = CGSize(width: view.bounds.width - 115, height: 10000)
+//            addressLabel.sizeToFit()
+//            addressLabel.frame.origin.x  = view.bounds.size.width - addressLabel.frame.size.width - 15
+            
+
+            return addressLabel.frame.size.height + 40
+            
+        } else {
+            return 44
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
