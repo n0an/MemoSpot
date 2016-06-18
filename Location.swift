d60bf5944399d029459112b0cfde5185298f5209
n0an <nantog@gmail.com>
2016-06-18 15:36:20 +0300

    CoreData adding: Model added Location NSManadedObject added

---
+++ Location.swift
@@ -0,0 +1,17 @@
+//
+//  Location.swift
+//  MyLocations
+//
+//  Created by Anton Novoselov on 18/06/16.
+//  Copyright © 2016 Anton Novoselov. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Location: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
