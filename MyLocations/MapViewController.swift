24efb9748532215965f17711169d11c2b982dc1f
n0an <nantog@gmail.com>
2016-06-18 22:28:24 +0300

    Map scene added to storyboard MapViewController started construction

---
+++ MyLocations/MapViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MapViewController.swift
+//  MyLocations
+//
+//  Created by Anton Novoselov on 18/06/16.
+//  Copyright © 2016 Anton Novoselov. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreData
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var managedObjectContext: NSManagedObjectContext!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    
+    @IBAction func showUser() {
+        let region = MKCoordinateRegionMakeWithDistance(mapView.userLocation.coordinate, 1000, 1000)
+        mapView.setRegion(mapView.regionThatFits(region), animated: true)
+    }
+    
+    @IBAction func showLocation() {
+        
+    }
+
+}
+
+
+extension MapViewController: MKMapViewDelegate {
+    
+}
